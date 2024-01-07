430465bc12bd09568beba81fd32899d1e3a8ec88
Daniel Herrera <daniel97hh@gmail.com>
2024-01-06 19:31:36 -0500

    chore: update categories skeleton

--- src/app/menu/loading.tsx
+++ src/app/menu/loading.tsx
@@ -5,9 +5,7 @@ export default function Loading() {
         <div className="flex items-center overflow-x-auto gap-9">
           
           {Array(6).fill(0).map((_, i) => (
-            <div className="flex flex-col items-center justify-between gap-2 pb-2 cursor-pointer" key={i}>
-              <div className="w-20 h-4 rounded-full bg-gray-300 animate-pulse"></div>
-            </div>
+            <div key={i} className="w-20 h-4 rounded-full bg-gray-300 animate-pulse"></div>
           ))}
         </div>
       </div>
