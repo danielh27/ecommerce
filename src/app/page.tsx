ad606f36c297e10c77256f58b9b5a381ad1c6691
Daniel Herrera <daniel97hh@gmail.com>
2024-01-17 01:44:34 -0500

    remove container from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const categories = await getAllCategories();
   
   return (
-    <div className='w-full h-full bg-white container mx-auto px-3 sm:px-5 mt-5'>
+    <div className='w-full h-full bg-white mx-auto px-3 sm:px-5 mt-5'>
       <Label title="MENÚ" divider={true} textClass='text-xl font-medium' />
       <HomeCategories categories={categories} />
     </div>
