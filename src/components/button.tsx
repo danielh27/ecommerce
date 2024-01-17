4af4ede3dca96d8ecf01b30de6c2f6d35a2c9e4e
Daniel Herrera <daniel97hh@gmail.com>
2024-01-17 01:24:22 -0500

    feat: add addToCart button (just visual)

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,12 +3,14 @@ import clsx from "clsx";
 interface ButtonProps {
   children?: React.ReactNode;
   width?: "full" | "fit-content";
+  classes?: string;
   onClick?: () => void;
 }
 
 const Button = ({
   children,
   width,
+  classes,
   onClick
 }: ButtonProps) => {
   
@@ -17,7 +19,8 @@ const Button = ({
       onClick={onClick}
       className={
         clsx("flex items-center justify-center px-4 py-2 bg-neutral-800 text-white rounded-full hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-700",
-        width === "full" ? "w-full" : "w-fit")}
+        width === "full" ? "w-full" : "w-fit",
+        classes)}
     >
       {children}
     </button>
--- src/components/product/form.tsx
+++ src/components/product/form.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@/interfaces/products";
 import ModifierGroup from "./modifier-group";
 import RelatedProductsGroup from "./related-products-group";
+import Button from "../button";
 
 type FormProps = {
   product: Product;
@@ -8,7 +9,7 @@ type FormProps = {
 
 const Form: React.FC<FormProps> = ({ product }) => {
   return (
-    <div className="flex flex-col bg-white rounded-2xl p-4">
+    <form className="flex flex-col bg-white rounded-2xl p-4">
       {product.modifier_groups.map((modifier_group) => (
         <ModifierGroup key={modifier_group.id} modifier_group={modifier_group} />
       ))}
@@ -16,7 +17,33 @@ const Form: React.FC<FormProps> = ({ product }) => {
       {product.related_products.map((related_products_group) => (
         <RelatedProductsGroup key={related_products_group.id} related_products_group={related_products_group} />
       ))}
-    </div>
+      
+      <div className="flex justify-center sm:justify-end w-dvw gap-5 bg-white border-t border-t-gray-300 fixed bottom-0 left-0 py-4 sm:pr-10">
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              data-action="decrease"
+              className="flex items-center justify-center w-10 h-10 text-2xl text-gray-500 focus:outline-none border rounded-full border-gray-500 bg-white" >
+              -
+            </button>
+            
+            <input
+              type="number"
+              defaultValue={0}
+              className="bg-transparent w-7 font-semibold text-center [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+              />
+            
+            <button
+              type="button"
+              data-action="increase"
+              className="flex items-center justify-center w-10 h-10 text-2xl text-gray-500 focus:outline-none border rounded-full border-gray-500" >
+              +
+            </button>
+          </div>
+          
+          <Button classes="py-3 px-4 bg-rose-600">Agregar {product.display_price}</Button>
+      </div>
+    </form>
   )
 }
 
